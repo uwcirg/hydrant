d7fa24ac7aa688edb10fb110f092ed0c21134e9e
Paul Bugni <pbugni@u.washington.edu>
2021-09-12 10:54:01 -0700

    add common /settings view to confirm deployed values

--- hydrant/views.py
+++ hydrant/views.py
@@ -1,5 +1,6 @@
 import click
-from flask import Blueprint, current_app
+from flask import Blueprint, abort, current_app, jsonify
+from flask.json import JSONEncoder
 import requests
 
 from hydrant.models.bundle import Bundle
@@ -11,6 +12,37 @@ base_blueprint = Blueprint('base', __name__, cli_group=None)
 @base_blueprint.route('/')
 def root():
     return {"message": "ok"}
+
+
+@base_blueprint.route('/settings', defaults={'config_key': None})
+@base_blueprint.route('/settings/<string:config_key>')
+def config_settings(config_key):
+    """Non-secret application settings"""
+
+    # workaround no JSON representation for datetime.timedelta
+    class CustomJSONEncoder(JSONEncoder):
+        def default(self, obj):
+            return str(obj)
+    current_app.json_encoder = CustomJSONEncoder
+
+    # return selective keys - not all can be be viewed by users, e.g.secret key
+    blacklist = ('SECRET', 'KEY')
+
+    if config_key:
+        key = config_key.upper()
+        for pattern in blacklist:
+            if pattern in key:
+                abort(status_code=400, messag=f"Configuration key {key} not available")
+        return jsonify({key: current_app.config.get(key)})
+
+    config_settings = {}
+    for key in current_app.config:
+        matches = any(pattern for pattern in blacklist if pattern in key)
+        if matches:
+            continue
+        config_settings[key] = current_app.config.get(key)
+
+    return jsonify(config_settings)
 
 
 @base_blueprint.cli.command("upload")
